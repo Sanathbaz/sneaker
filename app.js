a17f0d04f137cd6421263d6b2777101344671109
Sanathbaz <141004839+Sanathbaz@users.noreply.github.com>
2025-05-26 17:38:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
-const wrapper=document.querySelector(".slideWrapper")
-const menuitem= document.querySelectorAll(".menuitem");
-
+const wrapper = document.querySelector(".slideWrapper");
+const menuItems = document.querySelectorAll(".menuitem");
 
 const products = [
-   {
-     id: 1,
-     title: "Air Force",
-     price: 124,
-     colors: [
-       {
-         code: "black",
-         img: "./img/air.png",
-       },
-       {
-         code: "darkblue",
-         img: "./img/air2.png",
-       },
-     ],
-   },
-   {
-     id: 2,
-     title: "Jordan",
-     price: 110,
-     colors: [
-       {
-         code: "lightgray",
-         img: "./img/jordan.png",
-       },
-       {
-         code: "green",
-         img: "./img/jordan2.png",
-       },
-     ],
-   },
-   {
-     id: 3,
-     title: "CRATER",
-     price: 199,
-     colors: [
-       {
-         code: "lightgray",
-         img: "./img/crater.png",
-       },
-       {
-         code: "green",
-         img: "/img/crater.png",
-       },
-     ],
-   },
-   {
-     id: 4,
-     title: "BLAZER",
-     price: 99,
-     colors: [
-       {
-         code: "black",
-         img: "img/blazer2.png",
-       },
-       {
-         code: "lightgray",
-         img: "img/jordan2.png",
-       },
-     ],
-   },]
+  {
+    id: 1,
+    title: "Air Force",
+    price: 124,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Jordan",
+    price: 110,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "CRATER",
+    price: 199,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/crater.png",
+      },
+      {
+        code: "green",
+        img: "./img/crater2.png", // corrected potential typo
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "BLAZER",
+    price: 99,
+    colors: [
+      {
+        code: "black",
+        img: "./img/blazer2.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+];
 
-   let choosenProduct = products[0];
+let choosenProduct = products[0];
 
 const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
@@ -72,49 +72,51 @@ const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".size");
 
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    // Change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
-menuitem.forEach((item,index)=>{
-   item.addEventListener("click",()=>{
-    //change the current slide
-    wrapper.style.transform = `translateX( ${-125*index}vw)`;
+    // Change the chosen product
+    choosenProduct = products[index];
 
-    //change the choosen product
-    choosenProduct=products[index]
+    // Update product content
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
 
-    //change texts of currentProduct
-
-    currentProductTitle.textContent=choosenProduct.title;
-    currentProductPrice.textContent= "$" + choosenProduct.title;
-    currentProductImg.src=choosenProduct.colors[0].img;
-
-      //assing new colors
-    currentProductColors.forEach((color, index) => {
-      color.style.backgroundColor = choosenProduct.colors[index].code;
-  
-
-   });
+    // Update product color options
+    currentProductColors.forEach((color, i) => {
+      if (choosenProduct.colors[i]) {
+        color.style.backgroundColor = choosenProduct.colors[i].code;
+        color.style.display = "inline-block";
+      } else {
+        color.style.display = "none";
+      }
+    });
   });
 });
-currentProductSizes.forEach((size, index) => {
+
+// Update selected size UI
+currentProductSizes.forEach((size) => {
   size.addEventListener("click", () => {
-    currentProductSizes.forEach((size) => {
-      size.style.backgroundColor = "white";
-      size.style.color = "black";
+    currentProductSizes.forEach((s) => {
+      s.style.backgroundColor = "white";
+      s.style.color = "black";
     });
     size.style.backgroundColor = "black";
     size.style.color = "white";
   });
 });
 
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
 
-const productButton= document.querySelector(".productButton");
-const payment= document.querySelector(".payment");
-const close= document.querySelector(".close");
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
 
-productButton.addEventListener("click",()=>{
-  payment.style.display="flex"
-})
-close.addEventListener("click",()=>{
-  payment.style.display="none"
-
-})
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
